ccd7e67c6f3cd2a50e39c447d6b7562ec778cdf3
Kaique <Kaique@192.168.141.150>
2016-09-05 08:18:02 -0300

    Commit 2

--- src/java/CONEXAO/ConnectionFactory.java
+++ src/java/CONEXAO/ConnectionFactory.java
@@ -13,7 +13,7 @@ import java.sql.SQLException;
 public class ConnectionFactory {
     
     private static final String DRIVER = "com.mysql.jdbc.Driver";
-    private static final String URL = "jdbc:mysql://localhost:3306/sppda";
+    private static final String URL = "jdbc:mysql://localhost:3306/sppd";
     private static final String USER = "root";
     private static final String PASS = "2601";
     
